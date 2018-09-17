1ccc5a158c594acecf5ba75cd9aa8c8bdfce6d4f
Grzegorz Wasylow <grzegorz.wasylow@aon.pl>
2018-09-17 13:52:18 +0200

    Default routing & dashboards for application (including login and logout page).

---
+++ ic-elp/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
+import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
+import { EventLogComponent } from './event-log/event-log.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'event-log', component: EventLogComponent },
+  { path: 'hr-dashboard', component: HrDashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'manager-dashboard', component: ManagerDashboardComponent }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {
+}
--- ic-elp/src/app/app.module.ts
+++ ic-elp/src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
+import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
+import { EventLogComponent } from './event-log/event-log.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ManagerDashboardComponent,
+    HrDashboardComponent,
+    EventLogComponent,
+    LoginComponent,
+    LogoutComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
