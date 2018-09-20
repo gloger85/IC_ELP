52c336973f3b161fb16b5d7e060f346bd8ad6e7b
Andrzej Jabcoń <andrzej.jabcon@gmail.com>
2018-09-20 14:17:07 +0200

    Navigation after login to proper dashboard (hr-dashboard, manager-dashboard, dashboard) - part 1

--- ic-elp/src/app/login/login.component.ts
+++ ic-elp/src/app/login/login.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  private _loginRoutingMap : Map<string,string> = new Map([
+    ['hr@infosys.com', 'hr-dashboard'],
+    ['manager@infosys.com','manager-dashboard'],
+    ['others', 'dashboard']
+    ]);
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  login(email : string) : void {
+    
+    console.log('Email entered: ' + email);
+
+    email = email.trim();
+    if (!email) { 
+      console.log('Email not provided');
+      return;
+     }
+    ///TODO: Email validation (@infosys.com)
+
+    console.log('Navigate to: ' + this.getDashboardUrlByEmail(email));
+    
+    ///TODO: Navigate to the login page with extras
+    //this.router.navigate(['/'+]);
+  }
+
+  getDashboardUrlByEmail(email : string) : string {
+    
+    var dashboardUrl : string = this._loginRoutingMap.get(email);
+
+    if(dashboardUrl){
+      return dashboardUrl;
+    }else{
+      return this._loginRoutingMap.get('others');
+    }
+  } 
 }
