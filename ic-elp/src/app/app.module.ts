75f0fa4dbc5a04c591f2a43974495bde84f3c946
GregW <g.wasylow@gmail.com>
2018-09-27 15:11:04 +0200

    App.module review - correct order of imports and modules load list, pleae keep attnetion to a certain naming rules.

--- ic-elp/src/app/app.module.ts
+++ ic-elp/src/app/app.module.ts
@@ -1,70 +1,78 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { MegaMenuModule } from 'primeng/megamenu';
-import { AccordionModule } from 'primeng/accordion';
+
+/**
+ * PLEASE KEEP IMPORTS IN A CORRECT ORDER, THANK YOU!
+ */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CardModule } from 'primeng/card';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { ChartModule } from 'primeng/chart';
+import { NgModule } from '@angular/core';
+
+
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
-import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
 import { EventLogComponent } from './event-log/event-log.component';
+import { HrDashboardComponent } from './hr-dashboard/hr-dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
-import { AboutComponent } from './about/about.component';
+import { ManagerDashboardComponent } from './manager-dashboard/manager-dashboard.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { PasswordCreateNewComponent } from './password-create-new/password-create-new.component';
-import { SharedDetailInfoBarComponent } from './shared-detail-info-bar/shared-detail-info-bar.component';
-import { SharedTeamViewerComponent } from './shared-team-viewer/shared-team-viewer.component';
-import { SharedDashboardMenuComponent } from './shared-dashboard-menu/shared-dashboard-menu.component';
-import { SharedCalendarComponent } from './shared-calendar/shared-calendar.component';
 import { RequestLeaveComponent } from './request-leave/request-leave.component';
+import { SharedCalendarComponent } from './shared-calendar/shared-calendar.component';
+import { SharedDetailInfoBarComponent } from './shared-detail-info-bar/shared-detail-info-bar.component';
+import { SharedDashboardMenuComponent } from './shared-dashboard-menu/shared-dashboard-menu.component';
+import { SharedTeamViewerComponent } from './shared-team-viewer/shared-team-viewer.component';
 import { SharedMemberLeaveDetailsComponent } from './shared-member-leave-details/shared-member-leave-details.component';
+
+
+import { AccordionModule } from 'primeng/accordion';
+import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
-import { PanelModule } from 'primeng/panel';
+import { CardModule } from 'primeng/card';
+import { ChartModule } from 'primeng/chart';
+import { MegaMenuModule } from 'primeng/megamenu';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
+import { PanelModule } from 'primeng/panel';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AboutComponent,
     DashboardComponent,
-    ManagerDashboardComponent,
     HrDashboardComponent,
     EventLogComponent,
     LoginComponent,
     LogoutComponent,
-    AboutComponent,
+    ManagerDashboardComponent,
     PasswordResetComponent,
     PasswordCreateNewComponent,
+    RequestLeaveComponent,
     SharedDetailInfoBarComponent,
     SharedTeamViewerComponent,
     SharedDashboardMenuComponent,
     SharedCalendarComponent,
-    RequestLeaveComponent,
-    SharedMemberLeaveDetailsComponent
+    SharedMemberLeaveDetailsComponent,
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    ButtonModule,
-    MegaMenuModule,
     AccordionModule,
+    BrowserModule,
     BrowserAnimationsModule,
+    ButtonModule,
     CardModule,
-    ProgressSpinnerModule,
-    FormsModule,
     ChartModule,
     CalendarModule,
-    PanelModule,
+    FormsModule,
+    MegaMenuModule,
     MessagesModule,
-    MessageModule
+    MessageModule,
+    ProgressSpinnerModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
