5d75fa3092d5e22c4cfcddf909dad7da7e8c5d5d
GregW <g.wasylow@gmail.com>
2018-10-08 16:44:10 +0200

    Updates to Manager dashoard - Team Viewer now displays users and requests. READ ONLY MODE.

--- ic-elp/src/app/tree-test/tree-test.component.ts
+++ ic-elp/src/app/tree-test/tree-test.component.ts
@@ -6,60 +6,75 @@ import {TreeNode} from 'primeng/api';
   templateUrl: './tree-test.component.html',
   styleUrls: ['./tree-test.component.css']
 })
+
 export class TreeTestComponent implements OnInit {
 
-  files : TreeNode[];
-  selectedFiles: TreeNode[];
+  public files: TreeNode[];
+  public selectedFiles: TreeNode[];
 
   constructor() { }
 
   ngOnInit() {
     this.files = [
       {
-        "label": "Documents",
-        "data": "Documents Folder",
-        "expandedIcon": "fa fa-folder-open",
-        "collapsedIcon": "fa fa-folder",
-        "children": [{
-                "label": "Work",
-                "data": "Work Folder",
-                "expandedIcon": "fa fa-folder-open",
-                "collapsedIcon": "fa fa-folder",
-                "children": [{"label": "Expenses.doc", "icon": "fa fa-file-word-o", "data": "Expenses Document"}, {"label": "Resume.doc", "icon": "fa fa-file-word-o", "data": "Resume Document"}]
+        'label': 'Jan Kowalski',
+        'data': 'Jan Kowalski&#96;s files',
+        'expandedIcon': 'fa fa-folder-open',
+        'collapsedIcon': 'fa fa-folder',
+        'children': [{
+                'label': '2018',
+                'data': '2018',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
+                // tslint:disable-next-line:max-line-length
+                'children': [
+                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
             },
             {
-                "label": "Home",
-                "data": "Home Folder",
-                "expandedIcon": "fa fa-folder-open",
-                "collapsedIcon": "fa fa-folder",
-                "children": [{"label": "<b>Invoices.txt</b>", "icon": "fa fa-user", "data": "Invoices for this month"}]
+                'label': '2017',
+                'data': '2017',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
+                'children': [
+                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
             }]
     },
     {
-        "label": "Pictures",
-        "data": "Pictures Folder",
-        "expandedIcon": "fa fa-folder-open",
-        "collapsedIcon": "fa fa-folder",
-        "children": [
-            {"label": "barcelona.jpg", "icon": "fa fa-file-image-o", "data": "Barcelona Photo"},
-            {"label": "logo.jpg", "icon": "fa fa-file-image-o", "data": "PrimeFaces Logo"},
-            {"label": "primeui.png", "icon": "fa fa-file-image-o", "data": "PrimeUI Logo"}]
+      'label': 'Stefan Wojownik (Team Lead)',
+      'data': 'Stefan Wojwnik&#96;s files',
+      'expandedIcon': 'fa fa-folder-open',
+      'collapsedIcon': 'fa fa-folder',
+      'children': [{
+              'label': 'Alicja Pacynka',
+              'data': 'Alicja Pacynka&#96;s files',
+              'expandedIcon': 'fa fa-folder-open',
+              'collapsedIcon': 'fa fa-folder',
+              // tslint:disable-next-line:max-line-length
+              'children': [
+                {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
+          },
+          {
+              'label': 'Mariusz Czerkawski',
+              'data': 'Mariusz Czerkawski&#96;s files',
+              'expandedIcon': 'fa fa-folder-open',
+              'collapsedIcon': 'fa fa-folder',
+              // tslint:disable-next-line:max-line-length
+              'children': [
+                {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
+          }]
     },
     {
-        "label": "Movies",
-        "data": "Movies Folder",
-        "expandedIcon": "fa fa-folder-open",
-        "collapsedIcon": "fa fa-folder",
-        "children": [{
-                "label": "Al Pacino",
-                "data": "Pacino Movies",
-                "children": [{"label": "Scarface", "icon": "fa fa-file-video-o", "data": "Scarface Movie"}, {"label": "Serpico", "icon": "fa fa-file-video-o", "data": "Serpico Movie"}]
-            },
-            {
-                "label": "Robert De Niro",
-                "data": "De Niro Movies",
-                "children": [{"label": "Goodfellas", "icon": "fa fa-file-video-o", "data": "Goodfellas Movie"}, {"label": "Untouchables", "icon": "fa fa-file-video-o", "data": "Untouchables Movie"}]
-            }]
+        'label': 'Marianna Antonowska',
+        'data': 'Marianna Anotowska&#96;s files',
+        'expandedIcon': 'fa fa-folder-open',
+        'collapsedIcon': 'fa fa-folder',
+        'children': [
+          {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+          {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
     }
     ];
   }
