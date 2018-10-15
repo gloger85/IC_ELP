dce1dff9103ec8ec374389c9de876e6df1d20113
Andrzej Jabcoń <andrzej.jabcon@gmail.com>
2018-10-15 21:48:55 +0200

    shared-calendar fix - disabled public holidays in view; request-leave - added shared-member-leave-details in the top; leave-request-list - changed details button style to p-button

--- ic-elp/src/app/shared-calendar/shared-calendar.component.ts
+++ ic-elp/src/app/shared-calendar/shared-calendar.component.ts
@@ -37,10 +37,7 @@ export class SharedCalendarComponent implements OnInit {
 
     };
 
-    this.invalidDates = [new Date(), new Date('2018-11-01'), new Date('2018-11-11')];
-  }
-
-  Confirm() : void {
+    this.DisablePublicpublicHolidaysInCalendar(new Date().getMonth(), new Date().getFullYear());
   }
 
   CountNumberOfWorkingDays() : number {
@@ -49,23 +46,37 @@ export class SharedCalendarComponent implements OnInit {
       return 1;
     } 
 
-    let workDaysCount : number = 0;
+    let workingDaysCount : number = 0;
     let startDate : Date = this.rangeDates[0];
     let endDate : Date = this.rangeDates[1];
     let date : Date = new Date(startDate);
 
     while(date <= endDate){
-      if(!(date.getDay() in [0,6] || this.IsPublicHoliday(date))) workDaysCount++;
+      if(!(date.getDay() == 0 || date.getDay() == 6 || this.IsPublicHoliday(date))) workingDaysCount++;
       date.setDate(date.getDate() + 1);
     }
 
-    this.requestedDays = workDaysCount;
-    return workDaysCount;
+    this.requestedDays = workingDaysCount;
+    return workingDaysCount;
   }
 
-  IsPublicHoliday(date : Date) : boolean{
+  DisablePublicpublicHolidaysInCalendar(month : number, year : number) : void{
+    this.invalidDates = [];
+
+    for(let publicHoliday of this.GetPublicHolidaysByYear(year)){
+      this.invalidDates.push(new Date(year, publicHoliday[0], publicHoliday[1]));
+    }
+
+    if(month >= 11){ // Kalendarz wyswietlany na przelomie roku
+      for(let publicHoliday of this.GetPublicHolidaysByYear(year + 1)){
+        this.invalidDates.push(new Date(year + 1, publicHoliday[0], publicHoliday[1]));
+      }
+    }
+  }
+
+  GetPublicHolidaysByYear(year : number) : Array<number[]>{
     
-    let holidays : Array<number[]> = [ // [month][day]
+    let publicHolidays : Array<number[]> = [ // [month][day]
       [0,1],  // Nowy Rok
       [0,6],  // Trzech Króli
       [4,1],  // Święto Pracy
@@ -76,47 +87,54 @@ export class SharedCalendarComponent implements OnInit {
       [11,25],// Boże Narodzenie
       [11,26] // Boże Narodzenie
     ];
+  
+    // Obliczanie Wielkanocy
+    let a = year % 19;
+    let b = Math.floor(year / 100);
+    let c = year % 100;
+    let d = Math.floor(b / 4);
+    let e = b % 4;
+    let f = Math.floor((b + 8) / 25);
+    let g = Math.floor((b - f + 1) / 3);
+    let h = (19 * a + b - d - g + 15) % 30;
+    let i = Math.floor(c / 4);
+    let k = c % 4;
+    let l= (32+2*e+2*i-h-k)%7;
+    let m = Math.floor((a+11*h+22*l)/451);
+    let p = (h + l - 7 * m + 114) % 31;
+    let easterDay = p + 1;
+    let easterMonth = Math.floor((h + l - 7 * m + 114) / 31);
+
+    easterMonth--; // W JavaScript miesiące zaczynają się od 0 
+
+    let wielkanoc = new Date(year, easterMonth, easterDay);
+    let wielkanocnyPoniedzialek = new Date(year, easterMonth, easterDay+1);
+    let zeslanieDuchaSwietego = new Date(year, easterMonth, easterDay+49);
+    let bozeCialo = new Date(year, easterMonth, easterDay+60);
+
+    publicHolidays.push([wielkanoc.getMonth(), wielkanoc.getDate()]);
+    publicHolidays.push([wielkanocnyPoniedzialek.getMonth(), wielkanocnyPoniedzialek.getDate()]);
+    publicHolidays.push([zeslanieDuchaSwietego.getMonth(), zeslanieDuchaSwietego.getDate()]);
+    publicHolidays.push([bozeCialo.getMonth(), bozeCialo.getDate()]);  
+
+    return publicHolidays;
+  }
+
+  IsPublicHoliday(date : Date) : boolean{
+    
+    let publicHolidays : Array<number[]> = this.GetPublicHolidaysByYear(date.getFullYear());
 
     let month : number = date.getMonth();
     let day : number = date.getDate();     
-    
-    if (month >= 3 && month <= 6)
+
+    for(let publicHoliday of publicHolidays)
     {
-        // Obliczanie Wielkanocy
-        let a = date.getFullYear() % 19;
-        let b = Math.floor(date.getFullYear() / 100);
-        let c = date.getFullYear() % 100;
-        let d = Math.floor(b / 4);
-        let e = b % 4;
-        let f = Math.floor((b + 8) / 25);
-        let g = Math.floor((b - f + 1) / 3);
-        let h = (19 * a + b - d - g + 15) % 30;
-        let i = Math.floor(c / 4);
-        let k = c % 4;
-        let l= (32+2*e+2*i-h-k)%7;
-        let m = Math.floor((a+11*h+22*l)/451);
-        let p = (h + l - 7 * m + 114) % 31;
-        let easterDay = p + 1;
-        let easterMonth = Math.floor((h + l - 7 * m + 114) / 31);
-
-        easterMonth--; // W JavaScript miesiące zaczynają się od 0 
-
-        let wielkanoc = new Date(date.getFullYear(), easterMonth, easterDay);
-        let wielkanocnyPoniedzialek = new Date(date.getFullYear(), easterMonth, easterDay+1);
-        let zeslanieDuchaSwietego = new Date(date.getFullYear(), easterMonth, easterDay+49);
-        let bozeCialo = new Date(date.getFullYear(), easterMonth, easterDay+60);
-
-        holidays.push([wielkanoc.getMonth(), wielkanoc.getDate()]);
-        holidays.push([wielkanocnyPoniedzialek.getMonth(), wielkanocnyPoniedzialek.getDate()]);
-        holidays.push([zeslanieDuchaSwietego.getMonth(), zeslanieDuchaSwietego.getDate()]);
-        holidays.push([bozeCialo.getMonth(), bozeCialo.getDate()]);  
-    }
-
-    for(let holiday of holidays)
-    {
-      if(month == holiday[0] && day == holiday[1]) return true;
+      if(month == publicHoliday[0] && day == publicHoliday[1]) return true;
     }
 
     return false;
+  }
+
+  SendRequest() : void {
   }
 }
