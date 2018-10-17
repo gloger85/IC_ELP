d09b2548a068cc143e3d510ef2d34076a741d06e
Andrzej Jabcoń <andrzej.jabcon@gmail.com>
2018-10-17 11:20:11 +0200

    Separate compensation details component from leave details; Add tabs functionality for dashboard component; Add cancel button and request type dropdown for request leave component;

--- ic-elp/src/app/app.module.ts
+++ ic-elp/src/app/app.module.ts
@@ -22,8 +22,9 @@ import { RequestLeaveComponent } from './request-leave/request-leave.component';
 import { SharedCalendarComponent } from './shared-calendar/shared-calendar.component';
 import { SharedDetailInfoBarComponent } from './shared-detail-info-bar/shared-detail-info-bar.component';
 import { SharedDashboardMenuComponent } from './shared-dashboard-menu/shared-dashboard-menu.component';
-import { SharedTeamViewerComponent } from './shared-team-viewer/shared-team-viewer.component';
+import { SharedMemberCompensationDetailsComponent } from './shared-member-compensation-details/shared-member-compensation-details.component';
 import { SharedMemberLeaveDetailsComponent } from './shared-member-leave-details/shared-member-leave-details.component';
+import { SharedTeamViewerComponent } from './shared-team-viewer/shared-team-viewer.component';
 import { TreeTestComponent } from './tree-test/tree-test.component';
 
 import { AccordionModule } from 'primeng/accordion';
@@ -31,14 +32,15 @@ import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
 import { CardModule } from 'primeng/card';
 import { ChartModule } from 'primeng/chart';
+import { DropdownModule } from 'primeng/dropdown';
 import { MegaMenuModule } from 'primeng/megamenu';
 import { MessagesModule } from 'primeng/messages';
 import { MessageModule } from 'primeng/message';
 import { PanelModule } from 'primeng/panel';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { TableModule } from 'primeng/table';
+import {TabViewModule} from 'primeng/tabview';
 import { TreeModule } from 'primeng/tree';
-
 
 @NgModule({
   declarations: [
@@ -58,6 +60,7 @@ import { TreeModule } from 'primeng/tree';
     SharedTeamViewerComponent,
     SharedDashboardMenuComponent,
     SharedCalendarComponent,
+    SharedMemberCompensationDetailsComponent,
     SharedMemberLeaveDetailsComponent,
     TreeTestComponent
   ],
@@ -70,6 +73,7 @@ import { TreeModule } from 'primeng/tree';
     CardModule,
     ChartModule,
     CalendarModule,
+    DropdownModule,
     FormsModule,
     MegaMenuModule,
     MessagesModule,
@@ -77,6 +81,7 @@ import { TreeModule } from 'primeng/tree';
     ProgressSpinnerModule,
     PanelModule,
     TableModule,
+    TabViewModule,
     TreeModule
   ],
   providers: [ ],
