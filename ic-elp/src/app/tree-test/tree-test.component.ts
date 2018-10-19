088e382155fd422410097b2b524f0ddc7492f088
Grzegorz Wasylow <grzegorz.wasylow@aon.pl>
2018-10-19 09:30:35 +0200

    Manager dashboard update (member view changes requested by HR)

--- ic-elp/src/app/tree-test/tree-test.component.ts
+++ ic-elp/src/app/tree-test/tree-test.component.ts
@@ -17,31 +17,57 @@ export class TreeTestComponent implements OnInit {
   ngOnInit() {
     this.files = [
       {
-        'label': 'Jan Kowalski',
+        'label': 'Jan Kowalski (1 new)',
         'data': 'Jan Kowalski&#96;s files',
         'expandedIcon': 'fa fa-folder-open',
         'collapsedIcon': 'fa fa-folder',
         'children': [{
-                'label': '2018',
-                'data': '2018',
-                'expandedIcon': 'fa fa-folder-open',
-                'collapsedIcon': 'fa fa-folder',
-                // tslint:disable-next-line:max-line-length
-                'children': [
-                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
-                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
-            },
-            {
                 'label': '2017',
                 'data': '2017',
                 'expandedIcon': 'fa fa-folder-open',
                 'collapsedIcon': 'fa fa-folder',
+                // tslint:disable-next-line:max-line-length
+                'children': [
+                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2017 Parential Leave'},
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2017 Holidays'}]
+            },
+            {
+                'label': '2018 (1 new)',
+                'data': '2018',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
                 'children': [
                   {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
-                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
+                  {'label': '04/04/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '04/04/2018 Holidays'},
+                  {'label': '06/06/2018 Holidays (new)', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
             }]
-    },
-    {
+      },
+      {
+        'label': 'Roman Putas',
+        'data': 'Roman Putas&#96;s files',
+        'expandedIcon': 'fa fa-folder-open',
+        'collapsedIcon': 'fa fa-folder',
+        'children': [{
+                'label': '2017',
+                'data': '2017',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
+                // tslint:disable-next-line:max-line-length
+                'children': [
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
+            },
+            {
+                'label': '2018',
+                'data': '2018',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
+                'children': [
+                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'},
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '16/09/2018 Holidays'}]
+            }]
+        },
+    /*{
       'label': 'Stefan Wojownik (Team Lead)',
       'data': 'Stefan Wojwnik&#96;s files',
       'expandedIcon': 'fa fa-folder-open',
@@ -66,8 +92,8 @@ export class TreeTestComponent implements OnInit {
                 {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
                 {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
           }]
-    },
-    {
+    },*/
+    /*{
         'label': 'Marianna Antonowska',
         'data': 'Marianna Anotowska&#96;s files',
         'expandedIcon': 'fa fa-folder-open',
@@ -75,7 +101,34 @@ export class TreeTestComponent implements OnInit {
         'children': [
           {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
           {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
-    }
+    }*/
+      {
+        'label': 'Marianna Antonowska (2 new)',
+        'data': 'Marianna Antonowska&#96;s files',
+        'expandedIcon': 'fa fa-folder-open',
+        'collapsedIcon': 'fa fa-folder',
+        'children': [{
+                'label': '2017',
+                'data': '2017',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
+                // tslint:disable-next-line:max-line-length
+                'children': [
+                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                  {'label': '06/06/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'}]
+            },
+            {
+                'label': '2018 (2 new)',
+                'data': '2018',
+                'expandedIcon': 'fa fa-folder-open',
+                'collapsedIcon': 'fa fa-folder',
+                'children': [
+                  {'label': '01/02/2018 Parential Leave', 'icon': 'fa fa-file-word-o', 'data': '01/02/2018 Parential Leave'},
+                  {'label': '06/06/2018 Holidays (new)', 'icon': 'fa fa-file-word-o', 'data': '06/06/2018 Holidays'},
+                  {'label': '22/08/2018 Holidays (new)', 'icon': 'fa fa-file-word-o', 'data': '22/08/2018 Holidays'},
+                  {'label': '16/09/2018 Holidays', 'icon': 'fa fa-file-word-o', 'data': '16/09/2018 Holidays'}]
+            }]
+        },
     ];
   }
 }
