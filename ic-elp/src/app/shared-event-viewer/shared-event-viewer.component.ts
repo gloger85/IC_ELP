878b3cec33a45db12a7f4af0599919cc606d6ef7
Andrzej G <glogo85@gmail.com>
2018-10-19 11:16:29 +0200

    Added paging to event-log, corrected date fornat

--- ic-elp/src/app/shared-event-viewer/shared-event-viewer.component.ts
+++ ic-elp/src/app/shared-event-viewer/shared-event-viewer.component.ts
@@ -19,29 +19,71 @@ export class SharedEventViewerComponent implements OnInit {
     this.events = [
       {
         id: 1,
-        date: new Date(2017, 10, 10, 13, 10, 15),
+        date: new Date('2018-10-10'),
         user: 'Batman bin Superman',
         typeOfAction: 'ADD',
         change: 'Added new holiday request'
       },
       {
         id: 2,
-        date: new Date(2017, 10, 10, 13, 10, 15),
+        date: new Date('2018-10-09'),
         user: 'Iron Man',
         typeOfAction: 'UPDATE',
         change: 'Holiday request has been updated'
       },
       {
         id: 3,
-        date: new Date(2017, 10, 10, 13, 10, 15),
+        date: new Date('2018-10-08'),
         user: 'Luke Skywalker',
         typeOfAction: 'APPROVAL',
         change: 'Holiday request has been approved'
       },
       {
         id: 4,
-        date: new Date(2017, 10, 10, 13, 10, 15),
+        date: new Date('2018-10-07'),
         user: 'Luke Skywalker',
+        typeOfAction: 'APPROVAL',
+        change: 'Holiday request has been approved'
+      },
+      {
+        id: 5,
+        date: new Date('2018-10-06'),
+        user: 'Darth Vader',
+        typeOfAction: 'APPROVAL',
+        change: 'Holiday request has been approved'
+      },
+      {
+        id: 6,
+        date: new Date('2018-10-05'),
+        user: 'Yoda',
+        typeOfAction: 'APPROVAL',
+        change: 'Holiday request has been approved'
+      },
+      {
+        id: 7,
+        date: new Date('2018-10-04'),
+        user: 'Luke Skywalker',
+        typeOfAction: 'APPROVAL',
+        change: 'Holiday request has been approved'
+      },
+      {
+        id: 8,
+        date: new Date('2018-10-03'),
+        user: 'Black Widow',
+        typeOfAction: 'UPDATE',
+        change: 'Holiday request has been UPDATED'
+      },
+      {
+        id: 9,
+        date: new Date('2018-10-02'),
+        user: 'Hulk',
+        typeOfAction: 'APPROVAL',
+        change: 'Holiday request has been approved'
+      },
+      {
+        id: 10,
+        date: new Date(2017, 10, 10, 13, 10, 15),
+        user: 'Thor',
         typeOfAction: 'APPROVAL',
         change: 'Holiday request has been approved'
       }
