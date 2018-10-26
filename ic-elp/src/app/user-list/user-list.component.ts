3eed1b2096a7e7338f26c585ff9a129292989286
Andrzej Jabcoń <andrzej.jabcon@gmail.com>
2018-10-26 12:44:37 +0200

    user-list small fix :)

--- ic-elp/src/app/user-list/user-list.component.ts
+++ ic-elp/src/app/user-list/user-list.component.ts
@@ -14,10 +14,10 @@ export class UserListComponent implements OnInit {
 
   ngOnInit() {
     this.users = [
-      { id: '123', firstName: 'Andrzej', lastName: 'Dzabkon', team: 'South Africa', active: 'Yes'},
-      { id: '124', firstName: 'Grzegorz', lastName: 'Sylow', team: 'Aon 360', active: 'Yes'},
-      { id: '125', firstName: 'Bartosz', lastName: 'Granda', team: 'South Africa', active: 'Yes'},
-      { id: '126', firstName: 'Joanna', lastName: 'Kopacz', team: 'South Africa', active: 'Yes'},
+      { id: '123', firstName: 'Andrzej', lastName: 'Dzabkon', team: 'South Africa', active: 'Yes', email: 'andrzej.dzabkon@infosys.com'},
+      { id: '124', firstName: 'Grzegorz', lastName: 'Sylow', team: 'Aon 360', active: 'Yes', email: 'grzegorz.sylow@infosys.com'},
+      { id: '125', firstName: 'Bartosz', lastName: 'Granda', team: 'South Africa', active: 'Yes', email: 'bartosz.granda@infosys.com'},
+      { id: '126', firstName: 'Joanna', lastName: 'Kopacz', team: 'South Africa', active: 'Yes', email: 'joanna.kopacz@infosys.com'},
     ];
   }
 
